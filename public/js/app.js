e9f3fc3347b4252e648cc0b0e2f2ffa042074a15
Doug <util@rogers.com>
2016-11-24 11:42:07 -0500

    new Stuf

---
+++ public/js/app.js
@@ -0,0 +1,20 @@
+var socket = io(); //requires the socket.io file be loaded in the main HTML file first and creates a socket object and connects to the internal server
+socket.on('connect', function() {
+    console.log('Client connected to socket.io server');
+});
+socket.on('message', function(message) { //creates a listener for a custom event called in server.js
+    console.log('message recieved:' + message.text);
+});
+socket.emit('message2', { //access to the socket and pass data back. The first param is the event name, the second is the data being passed back. Using an object as the data allows you to store more.
+    text: "This is from the client"
+});
+//Handle message submit:
+var $form = jQuery('#message-form');
+$form.on('submit', function(event) {
+    event.preventDefault(); //keeps the form from submitting the old fashion ed way of refreshing the whole page
+    var $message = $form.find('input[name=message]'); //in jQuery using [] is way of specifying attributes on a object for searching
+    socket.emit('message', {
+        text: $message.val()
+    });
+    $message.val(''); //clear the input
+});
--- server.js
+++ server.js
@@ -4,8 +4,25 @@ var app = express();
 var http = require('http').Server(app); //creates a new serrver using the express app we crerated above as a boiler plate meaing whatever app listens to http does as well
 var io = require('socket.io')(http); //you pass it the http server variable. sockect.io expects this. The io variable is used like the app variable
 
-io.on('connection', function() { //on lets you liste to events. The first param is the name of the event and the second is the function to fire during that event
+io.on('connection', function(socket) { //on lets you liste to events. The first param is the name of the event and the second is the function to fire during that event. You get access to the individual socket connection
     console.log('user connected via socket.io'); //writes when a user connects
+
+    socket.on('message', function(message) { //listens to a message event
+        console.log('Message :' + message.text);
+        socket.broadcast.emit('message', message); //broadcast.emit send data to every socket except the one that originally sent it instead of every socket INCLUDING the originator
+
+    });
+
+    socket.on('message2', function(message) { //listens to a message event
+        console.log('Message from client :' + message.text);
+        socket.broadcast.emit('message2', message); //broadcast.emit send data to every socket except the one that originally sent it instead of every socket INCLUDING the originator
+
+    });
+
+
+    socket.emit('message', { //access to the socket and pass data back. The first param is the event name, the second is the data being passed back. Using an object as the data allows you to store more.
+        text: "Welcome to the app"
+    });
 });
 
 app.use(express.static(__dirname + '/public'));
